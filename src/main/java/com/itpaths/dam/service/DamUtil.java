a232d8b7afbee36e9b822cd74ab21da06d7be6de
Rishi Arora <rishia@cheneybrothers.com>
2020-07-04 18:30:42 -0400

    Updates

--- src/main/java/com/itpaths/dam/service/DamUtil.java
+++ src/main/java/com/itpaths/dam/service/DamUtil.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.sql.Timestamp;
 import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
 
 import static javax.script.ScriptEngine.FILENAME;
 
 @Service
 public class DamUtil {
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
     @Autowired
     private UtilConf utilConf;
     private String dfolder, sfolder;
@@ -27,18 +30,17 @@ public class DamUtil {
             if (sdir.isDirectory())
                 dir = true;
             else
-                error.append("Invalid source folder");
+                error.append("Invalid source folder. ");
         } catch (Exception e) {
-            error.append("Invalid source folder");
+            error.append("Invalid source folder. ");
         }
 
         if (dir) {
-            output.append(">>>>>>>>> " + utilConf.getUtilPath() + "\n ");
-            output.append(">>>>>>>>> " + MessageFormat.format(utilConf.getCommand(), sf, df) + "\n ");
+            output.append("Job placed successfully. ");
             Ope ope = new Ope();
             ope.start();
         } else {
-            error.append("<br>Invalid input");
+            error.append("Invalid input.");
         }
         String response = "{\"output\": \"" + output.toString().replaceAll("\b", "/") + "\", \"error\": \"" + error + "\" }";
         return response;
@@ -52,15 +54,15 @@ public class DamUtil {
             long startTime = System.currentTimeMillis();
             int i = 0;
             File lock = null;
-            File file = new File(sfolder + "\\import.log");
+            File file = new File(sfolder + "\\job-status.log");
             StringBuilder data = new StringBuilder();
             FileWriter lockw = null;
             try {
                 try {
-                    lock = new File(new File(sfolder).getParent() + "\\.lck");
+                    lock = new File(sfolder + "\\.lck");
                     lock.createNewFile();
                     lockw = new FileWriter(lock.getAbsoluteFile(), true);
-                    data.append("Lock file is crated");
+                    data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : Lock file is crated");
                     ProcessBuilder processBuilder = new ProcessBuilder();
                     processBuilder.command("cmd", "/c", MessageFormat.format(utilConf.getCommand(), sfolder, dfolder));
                     processBuilder.directory(new File(utilConf.getUtilPath()));
@@ -69,31 +71,32 @@ public class DamUtil {
                             new InputStreamReader(process.getInputStream()));
                     String line;
                     while ((line = reader.readLine()) != null) {
-                        data.append(line + "\n");
+                        data.append(sdf.format(new Timestamp(System.currentTimeMillis())) +" : " + line + "\n");
                     }
                     int exitVal = process.waitFor();
                     if (exitVal == 0) {
-                        data.append("Job completed successfully");
+                        data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : Job completed successfully");
                     } else {
-                        data.append("Something went wrong, fore more details, please check logs!");
+                        data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : Something went wrong, fore more details, please check logs!");
                     }
                 } catch (IOException e) {
-                    data.append(e.getMessage());
+                    data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : " + e.getMessage());
                     e.printStackTrace();
                 } catch (InterruptedException e) {
-                    data.append(e.getMessage());
+                    data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : " + e.getMessage());
                     e.printStackTrace();
                 }
             } catch (Exception e) {
-                data.append(e.getMessage());
+                    data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : " + e.getMessage());
                 e.printStackTrace();
             } finally {
                 try {
                     lockw.close();
                 } catch (Exception e) {
+                    data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : " + "No lock found");
                 }
                 lock.delete();
-                data.append("Lock file is removed");
+                data.append("\n sdf.format(new Timestamp(System.currentTimeMillis())) : Lock file is removed");
                 BufferedWriter bw = null;
                 FileWriter fw = null;
 
@@ -107,7 +110,7 @@ public class DamUtil {
                     bw = new BufferedWriter(fw);
                     bw.write(data.toString());
                 } catch (IOException e) {
-                    data.append(e.getMessage());
+                    data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : " + e.getMessage());
                     e.printStackTrace();
                 } finally {
                     try {
@@ -116,7 +119,7 @@ public class DamUtil {
                         if (fw != null)
                             fw.close();
                     } catch (IOException ex) {
-                        data.append(ex.getMessage());
+                        data.append("\n"+ sdf.format(new Timestamp(System.currentTimeMillis()))+" : " + ex.getMessage());
                         ex.printStackTrace();
                     }
                 }
