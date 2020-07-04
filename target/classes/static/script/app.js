da7794b0026b84fdffbdcf6af1ec20ad0f392749
umesh singh <umesh29k@gmail.com>
2020-07-04 22:32:34 +0530

    Updates

---
+++ target/classes/static/script/app.js
@@ -0,0 +1,5 @@
+document.write(unescape("%3Cscript src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js' type='text/javascript'%3E%3C/script%3E"));
+document.write(unescape("%3Cscript src='script/jquery.ztree.all.min.js' type='text/javascript'%3E%3C/script%3E"));
+document.write(unescape("%3Cscript src='script/jquery.ztree.core.min.js' type='text/javascript'%3E%3C/script%3E"));
+
+
